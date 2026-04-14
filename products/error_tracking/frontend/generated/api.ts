8d239504bdc2ab2fb6c6a41067a52cb722f6be7b
Catalin Irimie <catalin.i@posthog.com>
2026-04-14 15:35:58 +0300

    chore(error-tracking): update grouping rule create schema

--- products/error_tracking/frontend/generated/api.ts
+++ products/error_tracking/frontend/generated/api.ts
@@ -17,6 +17,7 @@ import type {
     ErrorTrackingFingerprintApi,
     ErrorTrackingFingerprintsListParams,
     ErrorTrackingGroupingRuleApi,
+    ErrorTrackingGroupingRuleCreateRequestApi,
     ErrorTrackingGroupingRuleListResponseApi,
     ErrorTrackingIssueFullApi,
     ErrorTrackingIssueMergeRequestApi,
@@ -439,14 +440,14 @@ export const getErrorTrackingGroupingRulesCreateUrl = (projectId: string) => {
 
 export const errorTrackingGroupingRulesCreate = async (
     projectId: string,
-    errorTrackingGroupingRuleApi: NonReadonly<ErrorTrackingGroupingRuleApi>,
+    errorTrackingGroupingRuleCreateRequestApi: ErrorTrackingGroupingRuleCreateRequestApi,
     options?: RequestInit
 ): Promise<ErrorTrackingGroupingRuleApi> => {
     return apiMutator<ErrorTrackingGroupingRuleApi>(getErrorTrackingGroupingRulesCreateUrl(projectId), {
         ...options,
         method: 'POST',
         headers: { 'Content-Type': 'application/json', ...options?.headers },
-        body: JSON.stringify(errorTrackingGroupingRuleApi),
+        body: JSON.stringify(errorTrackingGroupingRuleCreateRequestApi),
     })
 }
 
